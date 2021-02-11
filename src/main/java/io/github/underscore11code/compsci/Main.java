8f0e5b0ce53e2ca08a6756226e81131d94fffd02
underscore11code <minecrafter11mrt@gmail.com>
2021-02-11 12:10:31 -0800

    Printing

--- src/main/java/io/github/underscore11code/compsci/Main.java
+++ src/main/java/io/github/underscore11code/compsci/Main.java
@@ -2,14 +2,6 @@ package io.github.underscore11code.compsci;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
-
-    System.out.println(myInfo());
-  }
-
-  private static String myInfo() {
-    final StringBuilder sb = new StringBuilder();
-    sb.append("meow");
-    return sb.toString();
+    new Printing().run();
   }
 }
---
+++ src/main/java/io/github/underscore11code/compsci/Printing.java
@@ -0,0 +1,27 @@
+package io.github.underscore11code.compsci;
+
+public class Printing implements Runnable {
+  @Override
+  public void run() {
+    System.out.println("\n1. \n");
+    System.out.println("*  *  *  *  *");
+    System.out.println("*  *  *  *  *");
+    System.out.println("*  *  *  *  *");
+    System.out.println("*  *  *  *  *");
+    System.out.println("*  *  *  *  *");
+
+    System.out.println("\n2. \n");
+    System.out.println("* * * * *");
+    System.out.println("*       *");
+    System.out.println("*       *");
+    System.out.println("*       *");
+    System.out.println("* * * * *");
+
+    System.out.println("\n3. \n");
+    System.out.println("* * * * *");
+    System.out.println(" *     * ");
+    System.out.println("   * *   ");
+    System.out.println("    *    ");
+    System.out.println("    *    ");
+  }
+}
